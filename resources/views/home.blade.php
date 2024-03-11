1394109caebe8f389020a9df8153f099d6f6b6bc
PasquiFra <francesco.pasquinoni@gmail.com>
2024-03-11 18:08:36 +0100

    movies printed on page, some css improves

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,8 +12,26 @@
 
 </head>
 
-<body>
-
+<body class="bg-red">
+    <h1 class="text-center my-4">My Laravel Films</h1>
+    <div class="container">
+        <div class="row">
+            @foreach ($movies as $movie)
+            <div class="col-6">
+                <div class="card m-3">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$movie->title}}</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">Original title: {{$movie->original_title}}</h6>
+                        <div class="card-text">Production country: {{$movie->nationality}}</div>
+                        <div>Release date: {{$movie->date}}</div>
+                        <div>Avg_Vote: {{$movie->vote}}</div>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+        
 </body>
 
 </html>
